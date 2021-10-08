fc00edd0e3edf715eb4298a5008f81f2d0403758
fxu <feng.xu@screeningeagle.com>
2021-10-08 14:02:27 +0800

    add protocol

--- Sources/PackageWithoutSwiftExtension/PackageWithoutSwiftExtension.swift
+++ Sources/PackageWithoutSwiftExtension/PackageWithoutSwiftExtension.swift
@@ -1,3 +1,20 @@
+import Foundation
+
 struct PackageWithoutSwiftExtension {
-    var text = "Hello, World!"
+    var text = "PackageWithoutSwiftExtension"
+}
+
+public struct FooBarDataModel {
+    
+    public let id = UUID()
+    
+    public var name = "XYZ"
+    
+    public init() { }
+}
+
+public protocol FoobarDataConfigurable {
+    
+    func consumeDataFoo(_ data: FooBarDataModel)
+    func consumeDataBar(_ data: FooBarDataModel)
 }
